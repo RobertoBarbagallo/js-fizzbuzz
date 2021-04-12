ad6edd8313f69cee4916841e9b689dbc22a15c95
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-04-12 14:17:14 +0200

    file funzionante

---
+++ myPlugIn.js
@@ -0,0 +1,29 @@
+var i = 0;
+numero = 0;  
+var numeriDaUnoACento = [];
+
+for (i=0; i<100; i++){
+      numero = i + 1;
+      numeriDaUnoACento.push (numero);
+}
+
+console.log (numeriDaUnoACento);
+
+
+
+for (i=0; i<numeriDaUnoACento.length; i++){
+
+    
+
+    if (numeriDaUnoACento[i] % 3 === 0 && numeriDaUnoACento[i] % 5 !== 0){
+       numeriDaUnoACento.splice(i, 1, "Fizz")
+    } else if (numeriDaUnoACento[i] % 5 === 0 && numeriDaUnoACento[i] % 3 !== 0){
+        numeriDaUnoACento.splice(i, 1, "Buzz")
+    } else if (numeriDaUnoACento[i] % 3 === 0 && numeriDaUnoACento[i] % 5 === 0){
+        numeriDaUnoACento.splice(i, 1, "FizzBuzz")
+    } else{};
+}
+
+console.log (numeriDaUnoACento);
+
+document.getElementById("target").innerHTML = (numeriDaUnoACento.join(", "))
