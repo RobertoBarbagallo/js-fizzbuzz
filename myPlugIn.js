16bc49bb402f4b8081fd27ba021984aecfc058aa
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-04-12 14:34:18 +0200

    indentazione

--- myPlugIn.js
+++ myPlugIn.js
@@ -1,29 +1,33 @@
 var i = 0;
-numero = 0;  
+numero = 0;
 var numeriDaUnoACento = [];
 
-for (i=0; i<100; i++){
-      numero = i + 1;
-      numeriDaUnoACento.push (numero);
+for (i = 0; i < 100; i++) {
+    numero = i + 1;
+    numeriDaUnoACento.push(numero);
 }
 
-console.log (numeriDaUnoACento);
+console.log(numeriDaUnoACento);
+
+multiploDiTre = 0;
+multiploDiCinque = 0;
+
+for (i = 0; i < numeriDaUnoACento.length; i++) {
 
 
 
-for (i=0; i<numeriDaUnoACento.length; i++){
+    if (numeriDaUnoACento[i] % 3 === 0 && numeriDaUnoACento[i] % 5 !== 0) {
+        numeriDaUnoACento.splice(i, 1, "Fizz");
 
-    
+    } else if (numeriDaUnoACento[i] % 5 === 0 && numeriDaUnoACento[i] % 3 !== 0) {
+        numeriDaUnoACento.splice(i, 1, "Buzz");
 
-    if (numeriDaUnoACento[i] % 3 === 0 && numeriDaUnoACento[i] % 5 !== 0){
-       numeriDaUnoACento.splice(i, 1, "Fizz")
-    } else if (numeriDaUnoACento[i] % 5 === 0 && numeriDaUnoACento[i] % 3 !== 0){
-        numeriDaUnoACento.splice(i, 1, "Buzz")
-    } else if (numeriDaUnoACento[i] % 3 === 0 && numeriDaUnoACento[i] % 5 === 0){
-        numeriDaUnoACento.splice(i, 1, "FizzBuzz")
-    } else{};
+    } else if (numeriDaUnoACento[i] % 3 === 0 && numeriDaUnoACento[i] % 5 === 0) {
+        numeriDaUnoACento.splice(i, 1, "FizzBuzz");
+
+    } else { };
 }
 
-console.log (numeriDaUnoACento);
+console.log(numeriDaUnoACento);
 
-document.getElementById("target").innerHTML = (numeriDaUnoACento.join(", "))
+document.getElementById("target").innerHTML = (numeriDaUnoACento.join(",  "))
