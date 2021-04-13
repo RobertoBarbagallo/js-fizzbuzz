7538787f1845d0d5d314bdf8c5ae32f330166a8f
RobertoBarbagallo <roberto-barbagallo@live.it>
2021-04-13 10:02:27 +0200

    aggiunge var

--- myPlugIn.js
+++ myPlugIn.js
@@ -1,5 +1,5 @@
 var i = 0;
-numero = 0;
+var numero = 0;
 var numeriDaUnoACento = [];
 
 for (i = 0; i < 100; i++) {
@@ -11,15 +11,15 @@ for (i = 0; i < 100; i++) {
     var multiploDiCinque = numeriDaUnoACento[i] % 5 === 0 ;
 
     if (multiploDiTre && !multiploDiCinque) {
-        numeriDaUnoACento[i]= ("Fizz");
+        numeriDaUnoACento[i]= "Fizz";
         // numeriDaUnoACento.splice(i, 1, "Fizz");
 
     } else if (multiploDiCinque && !multiploDiTre) {
-        numeriDaUnoACento[i]= ("Buzz");
+        numeriDaUnoACento[i]= "Buzz";
         // numeriDaUnoACento.splice(i, 1, "Buzz");
 
     } else if (multiploDiTre && multiploDiCinque) {
-        numeriDaUnoACento[i]= ("FizzBuzz");
+        numeriDaUnoACento[i]= "FizzBuzz";
         // numeriDaUnoACento.splice(i, 1, "FizzBuzz");
 
     } else { };
